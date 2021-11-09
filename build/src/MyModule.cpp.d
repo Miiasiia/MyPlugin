0a63c8a3610c10fb7e599cc4d828f8d508aee5b1
Mia Sia <miiasiia@qq.com>
2021-11-09 17:43:27 +0800

    first commit

---
+++ build/src/MyModule.cpp.d
@@ -0,0 +1,236 @@
+build/src/MyModule.cpp.o: src/MyModule.cpp src/plugin.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/rack.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/common.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/logger.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/math.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/string.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/system.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/random.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/network.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/jansson.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/jansson_config.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/asset.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/window.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/GL/glew.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/GLFW/glfw3.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanovg.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanovg_gl.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanovg_gl_utils.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanosvg.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/midi.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/helpers.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/plugin/Model.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/plugin/Plugin.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuOverlay.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/OpaqueWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/Widget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/color.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/event.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/common.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/blendish.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuItem.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Menu.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuEntry.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuLabel.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/PortWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/common.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/MultiLightWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/LightWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/TransparentWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Module.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Param.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Port.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Light.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/ParamQuantity.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/Quantity.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/ParamWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Tooltip.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/history.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/Scene.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/RackScrollWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ScrollWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ScrollBar.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/ZoomWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/RackWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/FramebufferWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/ModuleWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/CableWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Cable.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/MenuBar.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/componentlibrary.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgKnob.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/Knob.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/TransformWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/SvgWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/svg.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/CircularShadow.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgSlider.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SliderKnob.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgPort.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/ModuleLightWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgSwitch.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/Switch.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgScrew.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/OpenGlWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/SequentialLayout.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Label.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/List.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/TextField.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/PasswordField.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Slider.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuSeparator.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Button.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/IconButton.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ChoiceButton.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/RadioButton.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ProgressBar.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/AudioWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/LedDisplay.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/audio.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/RtAudio.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/MidiWidget.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/RackRail.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgButton.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgPanel.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Engine.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/ParamHandle.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/plugin/callbacks.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/common.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/functions.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/vector.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/sse_mathfun_extension.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/sse_mathfun.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/digital.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/fft.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/pffft.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/filter.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/fir.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/midi.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/minblep.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/ode.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/resampler.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/ringbuffer.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/window.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/speex/speex_resampler.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/speex/speexdsp_types.h \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/vumeter.hpp \
+ C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/approx.hpp
+src/plugin.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/rack.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/common.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/logger.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/math.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/string.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/system.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/random.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/network.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/jansson.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/jansson_config.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/asset.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/window.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/GL/glew.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/GLFW/glfw3.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanovg.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanovg_gl.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanovg_gl_utils.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/nanosvg.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/midi.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/helpers.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/plugin/Model.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/plugin/Plugin.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuOverlay.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/OpaqueWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/Widget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/color.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/event.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/common.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/blendish.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuItem.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Menu.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuEntry.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuLabel.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/PortWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/common.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/MultiLightWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/LightWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/TransparentWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Module.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Param.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Port.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Light.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/ParamQuantity.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/Quantity.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/ParamWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Tooltip.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/history.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/Scene.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/RackScrollWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ScrollWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ScrollBar.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/ZoomWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/RackWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/FramebufferWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/ModuleWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/CableWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Cable.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/MenuBar.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/componentlibrary.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgKnob.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/Knob.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/TransformWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/SvgWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/svg.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/CircularShadow.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgSlider.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SliderKnob.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgPort.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/ModuleLightWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgSwitch.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/Switch.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgScrew.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/widget/OpenGlWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/SequentialLayout.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Label.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/List.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/TextField.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/PasswordField.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Slider.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/MenuSeparator.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/Button.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/IconButton.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ChoiceButton.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/RadioButton.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/ui/ProgressBar.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/AudioWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/LedDisplay.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/audio.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/RtAudio.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/MidiWidget.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/RackRail.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgButton.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/app/SvgPanel.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/Engine.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/engine/ParamHandle.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/plugin/callbacks.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/common.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/functions.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/vector.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/sse_mathfun_extension.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/simd/sse_mathfun.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/digital.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/fft.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/pffft.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/filter.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/fir.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/midi.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/minblep.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/ode.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/resampler.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/ringbuffer.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/window.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/speex/speex_resampler.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/dep/include/speex/speexdsp_types.h:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/vumeter.hpp:
+C:/Users/Mia/Projects/vcv/Rack/Rack-SDK/include/dsp/approx.hpp:
